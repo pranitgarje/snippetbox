47540d3340406e229bca1a4ea6593689333147ed
Pranit Garje <pranitgarje237@gmail.com>
2026-06-08 23:28:52 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.pranitgarje.net
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	
+	w.Write([]byte("Hello from snippetbox"))
+}
+func snippetView(w http.ResponseWriter,r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow","GET")
+		http.Error(w,"Method not allowed",http.StatusMethodNotAllowed)
+		return
+	}
+	id,err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w,r)
+		return
+	}
+	fmt.Fprint(w,"Display a spcecific snippet with ID %d",id)
+}
+func snippetCreate(w http.ResponseWriter,r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow","POST")
+		w.Header().Set("Content-Type", "application/json") 
+		w.Header().Add("Cache-Control", "public") 
+		w.Header().Add("Cache-Control", "max-age=31536000") 
+		http.Error(w,"Method not allowed",http.StatusMethodNotAllowed)	
+
+		return
+	}
+	w.Write([]byte(`{"name":"Alex"}`))
+}
+func main() {
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/",home)
+	mux.HandleFunc("/snippet/view",snippetView)
+	mux.HandleFunc("/snippet/create",snippetCreate)
+	log.Print("Starting server on 0.0.0.0:3000") 
+	err := http.ListenAndServe("0.0.0.0:3000", mux)
+    log.Fatal(err) 
+}
