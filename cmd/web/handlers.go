bbf5fbc604e02c9bad169b13572a94ad4b22db3f
Pranit Garje <145798451+pranitgarje@users.noreply.github.com>
2026-06-10 18:47:31 +0000

    improved configuration management and error logging

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
     "strconv" 
 ) 
  
-func home(w http.ResponseWriter, r *http.Request) { 
+func (app *application) home(w http.ResponseWriter, r *http.Request) { 
     if r.URL.Path != "/" { 
         http.NotFound(w, r) 
         return 
@@ -37,9 +37,10 @@ func home(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello from Snippetbox")) 
 } 
  
-func snippetView(w http.ResponseWriter, r *http.Request) { 
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) { 
     id, err := strconv.Atoi(r.URL.Query().Get("id")) 
     if err != nil || id < 1 { 
+        app.errorLog.Print(err.Error())
         http.NotFound(w, r) 
         return 
     } 
@@ -47,7 +48,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id) 
 } 
  
-func snippetCreate(w http.ResponseWriter, r *http.Request) { 
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) { 
     if r.Method != http.MethodPost { 
         w.Header().Set("Allow", http.MethodPost) 
         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed) 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,17 +3,40 @@ package main
 import ( 
     "log" 
     "net/http" 
+    "flag"
+    "os"
 ) 
+type application struct {
+    errorLog *log.Logger
+    infoLog *log.Logger
+}
  
 func main() { 
+      infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+       errorLog := log.New(os.Stderr, "ERROR\t", 
+log.Ldate|log.Ltime|log.Lshortfile) 
+    addr := flag.String("addr",":3000", "HTTP network address")
+    // flag.String,flag.Int,flag.Bool,flag.Duration,flag.Float64-> Automatic type conversion of command line input
+    flag.Parse()
+    // Initialize a new instance of our application struct, containing the 
+    // dependencies.
+    app := &application{
+        errorLog: errorLog,
+        infoLog: infoLog,
+    }
     mux := http.NewServeMux() 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
     mux.HandleFunc("/", home) 
     mux.HandleFunc("/snippet/view", snippetView) 
     mux.HandleFunc("/snippet/create", snippetCreate) 
- 
-    log.Print("Starting server on :3000") 
-    err := http.ListenAndServe(":3000", mux) 
-    log.Fatal(err) 
+    srv := &http.Server{
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: mux,
+        
+    }
+    infoLog.Printf("Starting server on %s",*addr) 
+    err := srv.ListenAndServe() 
+    errorLog.Fatal(err) 
 }
